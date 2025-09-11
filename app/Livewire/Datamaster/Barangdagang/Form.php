ec1cf194936eadf17a93b642936bcc99f6b639a8
Andi Fajar Nugraha <andifajarlah@gmail.com>
2025-09-11 12:56:42 +0800

    Remove validation rules for barangSatuan and related fields in Barangdagang form to simplify submission process.

--- app/Livewire/Datamaster/Barangdagang/Form.php
+++ app/Livewire/Datamaster/Barangdagang/Form.php
@@ -39,12 +39,12 @@ class Form extends Component
     {
         $this->validate([
             'kode_akun_id' => 'required',
-            'barangSatuan' => 'required',
-            // 'barangSatuan.*.rasio_dari_terkecil' => 'required|numeric|min:1',
-            'barangSatuan.*.harga_jual' => 'required',
-            'barangSatuan.*.nama' => 'required',
-            'nama' => 'required',
-            'unit_bisnis' => 'required',
+            // 'barangSatuan' => 'required',
+            // // 'barangSatuan.*.rasio_dari_terkecil' => 'required|numeric|min:1',
+            // 'barangSatuan.*.harga_jual' => 'required',
+            // 'barangSatuan.*.nama' => 'required',
+            // 'nama' => 'required',
+            // 'unit_bisnis' => 'required',
         ]);
 
         DB::transaction(function () {
