010477f218eab0c3c762eec18f1285a4ae332a97
Andi Fajar Nugraha <andifajarlah@gmail.com>
2025-09-15 10:35:12 +0800

    Add Pasien

--- app/Livewire/Datamaster/Pasien/Form.php
+++ app/Livewire/Datamaster/Pasien/Form.php
@@ -3,9 +3,49 @@
 namespace App\Livewire\Datamaster\Pasien;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
+use App\Models\Pasien;
 
 class Form extends Component
 {
+    public $data, $previous;
+    public $nama, $ihs, $nik, $rm, $jenis_kelamin, $birth_place, $tanggal_lahir, $tanggal_daftar, $alamat, $no_hp;
+
+    public function submit()
+    {
+        $this->validate([
+            'nama' => 'required',
+            'nik' => 'required',
+            'jenis_kelamin' => 'required',
+            'tanggal_lahir' => 'required',
+            'alamat' => 'required',
+            'no_hp' => 'required',
+        ]);
+
+        DB::transaction(function () {
+            $this->data->nama = $this->nama;
+            $this->data->ihs = $this->ihs;
+            $this->data->nik = $this->nik;
+            $this->data->alamat = $this->alamat;
+            $this->data->jenis_kelamin = $this->jenis_kelamin;
+            $this->data->tanggal_lahir = $this->tanggal_lahir;
+            $this->data->no_hp = $this->no_hp;
+            $this->data->pengguna_id = auth()->id();
+            $this->data->save();
+            session()->flash('success', 'Berhasil menyimpan data');
+        });
+        $this->redirect($this->previous);
+    }
+
+    public function mount(Pasien $data)
+    {
+        $this->previous = url()->previous();
+        $this->data = $data;
+        $this->rm = $this->data->id;
+        $this->fill($this->data->toArray());
+        $this->tanggal_lahir = $this->data->tanggal_lahir->format('Y-m-d');
+    }
+    
     public function render()
     {
         return view('livewire.datamaster.pasien.form');
--- app/Livewire/Datamaster/Pasien/Index.php
+++ app/Livewire/Datamaster/Pasien/Index.php
@@ -3,11 +3,23 @@
 namespace App\Livewire\Datamaster\Pasien;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use Livewire\Attributes\Url;
+use App\Models\Pasien;
 
 class Index extends Component
 {
+    use WithPagination;
+
+    #[Url] 
+    public $cari, $exist = 1;
+
+
     public function render()
     {
-        return view('livewire.datamaster.pasien.index');
+        return view('livewire.datamaster.pasien.index', [
+            'data' => Pasien::where(fn($q) => $q->where('nama', 'like', '%' . $this->cari . '%')->orWhere('id', 'like', '%' . $this->cari . '%'))
+                ->orderBy('id')->paginate(10)
+        ]);
     }
 }
