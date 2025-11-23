606d05a3418b1d0a9743b3193e741b8305000c51
Andi Fajar Nugraha <andifajarlah@gmail.com>
2025-11-23 08:40:45 +0800

    Update footer version in default layout to v2025.11.23.1435 for consistency with application versioning.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -26,7 +26,7 @@
         <div id="content" class="app-content">
             @yield('content')
             <div id="footer" class="app-footer mx-0 px-0">
-                &copy; 2025 {{ config('app.name') }} v2025.05.04.1435 - {{ config('app.organization') }}
+                &copy; 2025 {{ config('app.name') }} v2025.11.23.1 - {{ config('app.organization') }}
             </div>
         </div>
 
