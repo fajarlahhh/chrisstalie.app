cbcd6030a5e1b85aad889c795505eaada5f556af
Andi Fajar Nugraha <andifajarlah@gmail.com>
2026-02-13 11:56:14 +0800

    Update discount calculation in kasir view to include additional discount field
    - Modify the total discount calculation to sum 'total_diskon_barang', 'total_diskon_tindakan', and 'diskon', improving the accuracy of financial reporting in the kasir view.

--- resources/views/livewire/kasir/data.blade.php
+++ resources/views/livewire/kasir/data.blade.php
@@ -77,7 +77,7 @@
                                             <strong>{{ number_format($row->total_harga_barang + $row->total_diskon_barang) }}</strong>
                                         </li>
                                         <li>Total Diskon :
-                                            <strong>{{ number_format($row->total_diskon_barang + $row->total_diskon_tindakan) }}</strong>
+                                            <strong>{{ number_format($row->total_diskon_barang + $row->total_diskon_tindakan + $row->diskon) }}</strong>
                                         </li>
                                         <li>Total Tagihan :
                                             <strong>{{ number_format($row->total_tagihan) }}</strong>
